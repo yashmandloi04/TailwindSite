33a48517ddf4cbadb07312cd0dd5f757a90f1dce
yashmandloi04 <mandloi.yash04official1@gmail.com>
2024-12-27 23:34:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
